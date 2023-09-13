81c704494519adaa58baa041727eb1d01268a6e8
Anurag Ranjan <anuragranjan125@gmail.com>
2023-09-13 18:42:17 +0530

    final submit

--- App.js
+++ App.js
@@ -22,6 +22,7 @@ allBtn.addEventListener("click", handleAllBtn);
 movieSearchBtn.addEventListener("click", () => handleSearch(inputByUser.value));
 
 async function callAPI() {
+  container.innerHTML = `<div class="loader"></div>`;
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US&page=${currPage}`
   );
